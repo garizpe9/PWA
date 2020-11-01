0e4011809530750f91260183005add3e1385a638
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-11-01 16:55:57 -0600

    fxing cache typo

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,16 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
-    "/style.css",
-    "/index.js",
-    "/manifest.webmanifest",
-    "/favicon.ico",
-    "/icons/icon-512x512.png",
-    "/icons/icon-192x192.png",
+     "/index.html",
+     "/styles.css",
+     "index.js",
+     "manifest.webmanifest",
+     "favicon.ico",
+     "icons/icon-512x512.png",
+     "icons/icon-192x192.png",
 
   ];
-  
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
   
   // install
   self.addEventListener("install", function (evt) {
